12d44576f19ac8ee2689a07526f770143c9a447c
lina0611 <linagao0611@gmail.com>
2019-09-26 12:47:35 -0400

    added MediaItemCollectionVC into category cell and ceate a new MediaItemCell

--- PlayList/CategoryCell.swift
+++ PlayList/CategoryCell.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CategoryCell: UICollectionViewCell {
 
+    private let cellIdentifier = "MediaItemCell"
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
@@ -19,9 +21,36 @@ class CategoryCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
 
-    
+    let mediaItemscollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView.init(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.blue
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
 
     func setupViews() {
         backgroundColor = UIColor.black
+
+        addSubview(mediaItemscollectionView)
+
+        mediaItemscollectionView.dataSource = self
+        mediaItemscollectionView.delegate = self
+
+        mediaItemscollectionView.register(MediaItemCell.self, forCellWithReuseIdentifier: cellIdentifier)
+
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": mediaItemscollectionView]))
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutConstraint.FormatOptions(), metrics: nil, views: ["v0": mediaItemscollectionView]))
+    }
+}
+
+extension CategoryCell: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
     }
 }
